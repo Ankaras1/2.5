d58c5d16f7844aee68b3dcc0e861b16cb36295af
Анастасия <tishenckanastas@gmail.com>
2024-09-08 16:36:40 +0700

    Add files via upload

---
+++ modulr_2.5.py
@@ -0,0 +1,18 @@
+def get_matrix(n, m, value):
+    matrix=[]
+    for i in range(n):
+        mat=[]
+        matrix.append(mat)
+        for j in range(m):
+            mat.append(value)
+    print(matrix)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+
+
+
+
+
+
